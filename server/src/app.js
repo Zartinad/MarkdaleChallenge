8d19a4ac155e92f5fd463aecd4f8a528285932b5
Adam <a.zartin@mail.utoronto.ca>
2019-04-15 21:34:31 -0400

    reformatted to incoporate forked btcutils

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors())
 
-app.listen(process.env.PORT || 8890)
+app.listen(process.env.PORT || 8891)
 
 //Routing
 
